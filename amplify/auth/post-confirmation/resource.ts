53cdec328d7db03152dd47f518183020dbb3afa3
Michael Petersen <mpetersen910@gmail.com>
2024-08-26 10:56:04 -0400

    update

--- amplify/auth/post-confirmation/resource.ts
+++ amplify/auth/post-confirmation/resource.ts
@@ -2,5 +2,4 @@ import { defineFunction } from '@aws-amplify/backend';
 
 export const postConfirmation = defineFunction({
   name: 'post-confirmation',
-  entry: './handler.ts', // Path to the handler file within the code directory
 });
--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -23,5 +23,4 @@ export const data = defineData({
       expiresInDays: 30,
     },
   },
-}
-);
+});
